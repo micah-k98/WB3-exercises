eb1a642f3f0700734ed5f17511c28bb5cfb5bdf5
micah-k98 <146677653+micah-k98@users.noreply.github.com>
2023-10-24 13:24:40 -0700

    Added whyImHere function q

--- Functions/simple_functions.js
+++ Functions/simple_functions.js
@@ -7,4 +7,12 @@ function favoriteThings()
     console.log("Kang Daniel");
 }
 
+function whyImHere() 
+{
+    console.log("I want to earn more experience in coding and by the end of it, be proficient enough to apply and get a job in tech industry.");
+}
+
+
 favoriteThings();
+whyImHere();
+
