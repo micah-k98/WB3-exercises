acd587900be9bd1356168989a208de5997059419
micah-k98 <146677653+micah-k98@users.noreply.github.com>
2023-10-24 14:44:50 -0700

    Defined functions

--- Functions/more_functions.js
+++ Functions/more_functions.js
@@ -2,14 +2,24 @@
 
 function main()
 {
+    displayMailingLabel("Micah Kashiwabara", "19221 N Pierson Rd.", "Sun City", "AZ", "85373");
+    displayMailingLabel("Anya Forger", "128 Park Avenue", "Spy City", "Westalis", "007");
     
-    
+    addNumbers(10,12);
+    addNumbers(6,98);
+
+    console.log("");
+
+    displayReceipt(98, 103);
+    displayReceipt(96, 96);
+    displayReceipt(101, 100);
 }
 
 function displayMailingLabel(name, address, city, state, zip)
 {
     console.log(name);
     console.log(`${address}, ${city}, ${state} ${zip}`);
+    console.log("")
 }
 
 function addNumbers(num1, num2)
@@ -25,8 +35,8 @@ function displayReceipt(totalDue, amountPaid)
     console.log("Total Due:   $" + totalDue);
     console.log("Amount Paid: $" + amountPaid);
     console.log("Change Due:  $" + changeDue);
+    console.log("")
 }
-
 
 
 main();
