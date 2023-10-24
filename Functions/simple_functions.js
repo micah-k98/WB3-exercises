3ce6540256bbeef8fa129c88aff10a80a2458b83
micah-k98 <146677653+micah-k98@users.noreply.github.com>
2023-10-24 13:29:25 -0700

    Added favoritePlace function

--- Functions/simple_functions.js
+++ Functions/simple_functions.js
@@ -12,7 +12,12 @@ function whyImHere()
     console.log("I want to earn more experience in coding and by the end of it, be proficient enough to apply and get a job in tech industry.");
 }
 
+function favoritePlace()
+{
+    console.log("I haven't been there, but my favorite place is Japan. There a lot of beautiful places and things to do there.");
+}
+
 
 favoriteThings();
 whyImHere();
-
+favoritePlace();
