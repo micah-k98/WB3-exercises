66a88c719c698e7da007b5b005c970bd19e10696
micah-k98 <146677653+micah-k98@users.noreply.github.com>
2023-10-26 01:35:40 -0700

    replace() with case insensitive works

--- Strings/replace_strings.js
+++ Strings/replace_strings.js
@@ -2,7 +2,8 @@
 
 let message = "Our corporate offices are located in Dallas";
 let newMessage = message.replace("Dallas", "Austin");
-
+let newMessageCaseInsensitive = message.replace(/DALLAS/i, "Austin");
 
 console.log(message);
 console.log("New message: " + newMessage);
+console.log("New message, case insensitive: " + newMessageCaseInsensitive);
