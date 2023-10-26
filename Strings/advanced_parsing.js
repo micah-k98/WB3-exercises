803848ceedbcc102244d092bcbb7cfc4194c2e79
micah-k98 <146677653+micah-k98@users.noreply.github.com>
2023-10-26 01:11:19 -0700

    Added first, middle and last name function.

--- Strings/advanced_parsing.js
+++ Strings/advanced_parsing.js
@@ -16,7 +16,10 @@ function parseAndDisplayName(givenName)
     {
         firstAndLastName(givenName, firstName, lastName);
     }
-    
+    else
+    {
+        firstMiddleLastName(givenName, firstName, middleName, lastName);
+    }
 }
 
 function onlyName(givenName)
@@ -32,9 +35,16 @@ function firstAndLastName(givenName, firstName, lastName)
     console.log("Last Name: " + lastName);
 }
 
+function firstMiddleLastName(givenName, firstName, middleName, lastName)
+{
+    console.log("Name: " + givenName);
+    console.log("First Name: " + firstName);
+    console.log("Middle Name: " + middleName);
+    console.log("Last Name: " + lastName);
+}
 
 
-parseAndDisplayName("Cher Auston");
+parseAndDisplayName("Cher Auston Smith");
 // parseAndDisplayName("Ian Auston");
 // parseAndDisplayName("Siddalee Grace");
 
