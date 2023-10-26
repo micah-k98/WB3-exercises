b96e1e88a18a42162d6dd787d28417d4708d9f86
micah-k98 <146677653+micah-k98@users.noreply.github.com>
2023-10-26 01:35:07 -0700

    replace() method works

---
+++ Strings/replace_strings.js
@@ -0,0 +1,8 @@
+"use strict"
+
+let message = "Our corporate offices are located in Dallas";
+let newMessage = message.replace("Dallas", "Austin");
+
+
+console.log(message);
+console.log("New message: " + newMessage);
